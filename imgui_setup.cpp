d690a4b570d675ab35a9e1f3e7885232bdb18746
Nicolas Hürlimann <nihuerlimann@ethz.ch>
2024-04-17 18:07:09 +0200

    support for linux

--- imgui_setup.cpp
+++ imgui_setup.cpp
@@ -9,6 +9,7 @@ ImFont* openSansBoldL;
 
 void ImGui::SetupImGui()
 {
+    #if _WIN32
     openSansS = ImGui::GetIO().Fonts->AddFontFromFileTTF(".\\fonts\\OpenSans-Regular.ttf",
         20.0f, NULL, ImGui::GetIO().Fonts->GetGlyphRangesDefault());
 
@@ -27,6 +28,28 @@ void ImGui::SetupImGui()
     openSansBoldL = ImGui::GetIO().Fonts->AddFontFromFileTTF(".\\fonts\\OpenSans-Bold.ttf",
         40.0f, NULL, ImGui::GetIO().Fonts->GetGlyphRangesDefault());
 
+    #elif __linux__
+    openSansS = ImGui::GetIO().Fonts->AddFontFromFileTTF("../fonts/OpenSans-Regular.ttf",
+        20.0f, NULL, ImGui::GetIO().Fonts->GetGlyphRangesDefault());
+
+    openSansM = ImGui::GetIO().Fonts->AddFontFromFileTTF("../fonts/OpenSans-Regular.ttf",
+        30.0f, NULL, ImGui::GetIO().Fonts->GetGlyphRangesDefault());
+
+    openSansL = ImGui::GetIO().Fonts->AddFontFromFileTTF("../fonts/OpenSans-Regular.ttf",
+        40.0f, NULL, ImGui::GetIO().Fonts->GetGlyphRangesDefault());
+
+    openSansBoldS = ImGui::GetIO().Fonts->AddFontFromFileTTF("../fonts/OpenSans-Bold.ttf",
+        20.0f, NULL, ImGui::GetIO().Fonts->GetGlyphRangesDefault());
+
+    openSansBoldM = ImGui::GetIO().Fonts->AddFontFromFileTTF("../fonts/OpenSans-Bold.ttf",
+        30.0f, NULL, ImGui::GetIO().Fonts->GetGlyphRangesDefault());
+
+    openSansBoldL = ImGui::GetIO().Fonts->AddFontFromFileTTF("../fonts/OpenSans-Bold.ttf",
+        40.0f, NULL, ImGui::GetIO().Fonts->GetGlyphRangesDefault());
+    
+
+
+    #endif
 
     ImGui::GetIO().Fonts->Build();
 }
